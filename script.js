41d14c521f51d4ee7e0c74716575eaf7b86b81f5
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-01-05 10:12:47 +0100

    Modif

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let spentAdd = document.getElementById("spaceAddD");
 let incomeAdd = document.getElementById("spaceAddR");
 let arraySpent = [];
 let arrayIncome = [];
+let total = document.getElementById("total");
 
 for(let x = 0; x < document.getElementsByClassName("spent").length; x++) {
     arraySpent.push(document.getElementsByClassName("spent")[x]);
@@ -30,6 +31,7 @@ function added(place, field, add, array) {
 
     div.className = "space";
     p.innerHTML = "€";
+    div.classList.add("addSpace");
 
     place.append(div);
     place.insertBefore(div, add);
@@ -41,12 +43,19 @@ function added(place, field, add, array) {
     input.pattern = "[0-9]{0,}.[0-9]{2}";
     input.placeholder = "0.00";
     array.push(input);
+
+    document.getElementById("reset").addEventListener("click", function () {
+        for(let x = 0; x < document.getElementsByClassName("addSpace").length; x++){
+            document.getElementsByClassName("addSpace")[x].remove();
+        }
+    });
 }
 
 document.getElementById("addD").addEventListener("click", function () {
     if(spentField.value.length >=  1) {
         added(spent, spentField, spentAdd, arraySpent);
         spentField.value = "";
+        console.log(document.getElementsByClassName("addSpace").length)
     }
 });
 
@@ -56,5 +65,23 @@ document.getElementById("addR").addEventListener("click", function () {
         incomeField.value = "";
     }
 });
+
+document.getElementById("calcul").addEventListener("click", function () {
+    result();
+});
+
+function result() {
+    total.innerHTML = "";
+    for(let x = 0; x < arraySpent.length; x++) {
+        console.log(parseFloat(arraySpent[x].value))
+        if(((isNaN(arraySpent[x]) === true) && (arraySpent[x].length > 0))) {
+            alert("Un champ n'est pas valide");
+        }
+
+        else if((parseFloat(arraySpent[x].value) % 1 !== 0) && (arraySpent[x].length > 0) && ((parseFloat(arraySpent[x].value) * 100) % 1 === 0) && ((parseFloat(arraySpent[x].value) * 10) % 1 !== 0)) {
+            total.innerHTML -= arraySpent[x].value;
+        }
+    }
+}
 
 
